13c5809579b7a6e8fe530b1d84e09e57eed9a8ed
Derrick <derricklpatterson@gmail.com>
2018-03-14 16:43:01 -0400

    added testVision method

--- foodSense.py
+++ foodSense.py
@@ -1,41 +1,28 @@
-# foodSense.py - Testing the Google Cloud Platform (GCP) Vision API with fridge door sensor acting as a trigger
-# Built utilizing GCP Vision API and python.org documentation
-
-# Include required packages
+# Include required modules
 import base64
+import io
 import json
 import os
 import time
 import sys
+
+# Hardware specific modules
 import RPi.GPIO as GPIO
-
-from google.cloud import vision
-import google.auth
-
-#from googleapiclient import discovery
-#from oauth2client.client import GoogleCredentials
-
 from picamera import PiCamera
 from scale import Scale
 
+# Google API and Authentication modules
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+
+from google.cloud import vision
+from google.cloud.vision import types
 
 def authenticate():
     print('Authenticating with Google Vision API')
-  
-    #GOOGLE_APPLICATION_CREDENTIALS = "~/gcp-service-account.json"
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "fs-service-account.json"
-    credentials, project = google.auth.default()
-    
-    if credentials.requires_scopes:
-        credentials = credentials.with_scopes(['https://vision.googleapis.com/v1/images:annotate'])
-    client = vision.Client(credentials=credentials)
-    buckets = client.list_buckets()
-    
-    for bkt in buckets:
-        print(bkt)
-
-    #credentials = GoogleCredentials.get_application_default()
-    #return discovery.build('vision', 'v1', credentials=credentials)
+    credentials = GoogleCredentials.get_application_default()
+    return discovery.build('vision', 'v1', credentials=credentials)
 
 
 def initScale():
@@ -112,6 +99,29 @@ def parse(response):
             print('Match found: ' + itemLabels[i])
 
 
+def testVision():
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources/wakeupcat.jpg')
+
+    # Loads the image into memory
+    with io.open('grannysmith.png', 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
 
 def main():
     DOOR = 27
@@ -119,9 +129,8 @@ def main():
     GPIO.setup(DOOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     
     #service = authenticate()
-    authenticate()
     scale = initScale()
-    
+
     try:
         while True:
             while True:# (GPIO.input(DOOR) == 1):
@@ -137,6 +146,7 @@ def main():
                     print("Door was closed")
                     
                     getWeight(scale)
+                    testVision()
                     #filename = getImage()
                     #response = detect(service, 'grannysmith.png')
                     #parse(response)
